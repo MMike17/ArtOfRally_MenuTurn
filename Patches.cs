60f9499f116862bdc0b5e378c3c30e75dd41f5ba
MMike17 <matthews.r.mike@gmail.com>
2025-01-02 16:05:48 +0100

    Automate car rotation

--- Patches.cs
+++ Patches.cs
@@ -1,20 +1,36 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 
 namespace MenuTurn
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// [HarmonyPatch(typeof(), MethodType.)]
-	// static class type_method_Patch
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
+    [HarmonyPatch(typeof(CarChooserManager), "Update")]
+    static class CarChooserManager_Update_Patch
+    {
+        static void Prefix(CarChooserManager __instance)
+        {
+            if (!Main.enabled)
+                return;
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+            float rotation = Main.settings.rotationSpeed * (Main.settings.reverseRotation ? -1 : 1);
+
+            foreach (CarMenuDisplay carMenuDisplay in __instance.AllCars)
+            {
+                if (carMenuDisplay.isSelected)
+                    Main.SetField(__instance, "currentCarRotation", BindingFlags.Instance, carMenuDisplay.RotateCar(rotation));
+            }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (!Main.enabled)
+                    yield return instruction;
+                else
+                    yield return new CodeInstruction(OpCodes.Ret);
+            }
+        }
+    }
 }
